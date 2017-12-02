7bb9395d3deeedf87cbc50139a0b35adb41d05bc
Isaiah Soung <isaiah131@gmail.com>
2017-12-02 02:33:45 -0600

    Added interface type to options object
    Added interface type to options object since 'options' & 'targets' don't belong to 'object'.

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,7 @@
-declare function loadClass(schema: any, {target: any,options: object}:object): void;
+interface ILoadClassOptions {
+  target: any;
+  options: object;
+}
+
+declare function loadClass(schema: any, {target: any , options: object}:ILoadClassOptions): void;
 export = loadClass;
